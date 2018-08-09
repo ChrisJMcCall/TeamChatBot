acee646623b988929242f3c9547c31d263fc01ff
Chris McCall <cmccall@cpat.com>
2018-08-08 22:40:34 -0500

    Setting up files to run.

---
+++ Controllers/MessagesController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Connector;
+using Microsoft.Extensions.Configuration;
+
+namespace teamsBot.Controllers
+{
+    public class MessagesController : Controller
+    {
+        private IConfiguration configuration;
+
+
+        public MessagesController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+
+        [Authorize(Roles = "Bot")]
+        [HttpPost]
+        public async Task<OkResult> Post([FromBody] Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                //MicrosoftAppCredentials.TrustServiceUrl(activity.ServiceUrl);
+                var appCredentials = new MicrosoftAppCredentials(configuration);
+                var connector = new ConnectorClient(new Uri(activity.ServiceUrl), appCredentials);
+
+                // return our reply to the user
+                var reply = activity.CreateReply("HelloWorld");
+                await connector.Conversations.ReplyToActivityAsync(reply);
+            }
+            else
+            {
+                //HandleSystemMessage(activity);
+            }
+            return Ok();
+        }
+
+    }
+
+}
--- Startup.cs
+++ Startup.cs
@@ -4,18 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.Bot.Connector;
 
 namespace teamsBot
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appSettings.json", optional:true, reloadOnChange:true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
@@ -23,7 +31,28 @@ namespace teamsBot
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddSingleton(_ => Configuration);
+
+            var credentialProvider = new StaticCredentialProvider(
+                Configuration.GetSection(MicrosoftAppCredentials.MicrosoftAppIdKey)?.Value,
+                Configuration.GetSection(MicrosoftAppCredentials.MicrosoftAppPasswordKey)?.Value
+            );
+
+           services.AddAuthentication(
+                    options =>
+                    {
+                        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                    }
+                )
+                .AddBotAuthentication(credentialProvider);
+            
+            services.AddSingleton(typeof(ICredentialProvider), credentialProvider);
+
+            services.AddMvc(options =>
+            {
+                options.Filters.Add(typeof(TrustServiceUrlAttribute));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +63,8 @@ namespace teamsBot
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+            app.UseAuthentication();
             app.UseMvc();
         }
     }
